5f40d5293a866cbf5196b4421ac2b2955ef9e70c
leordancarmona07 <leordan.carmona@student.passerellesnumeriques.org>
2021-01-20 21:20:41 +0800

    Commiting Homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { SignInComponent } from './Authentication/sign-in/sign-in.component';
 import { SignUpComponent } from './Authentication/sign-up/sign-up.component';
 import { AboutComponent } from './Public/Views/about/about.component';
+import { HomeComponent } from './Public/Views/home/home.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'sign-in', component: SignInComponent },
   { path: 'sign-up', component: SignUpComponent},
   { path: 'about', component: AboutComponent}
